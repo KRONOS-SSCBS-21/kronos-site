cfe896d72cad1b42f5de0f8dbee5093afeb1b7d7
Ajaykumar2005 <98280155+Ajaykumar2005@users.noreply.github.com>
2023-02-20 21:50:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import aboutus from "../public/Component/aboutus/aboutus";
+
+function App() {
+  return (
+    <div className="App" >
+      <aboutus></aboutus>
+    </div>
+  );
+}
+
+export default App;
